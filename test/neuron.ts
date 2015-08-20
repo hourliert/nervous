6158c777ee6599785dcaf2934a4982fcde5a8e19
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-08-20 09:19:17 -0700

    Unit testing for neuron class.

--- test/neuron.ts
+++ test/neuron.ts
@@ -26,6 +26,7 @@ describe('Neuron', () => {
   });
   
   it ('should be defined', () => {
+    //all abstraction have no overrided method. so we only need to test the Neuron class.
     expect(n).to.be.ok;
     expect(n.id).to.equal(`n_${layer.id}_0`);
     expect(bias).to.be.ok;
@@ -37,4 +38,119 @@ describe('Neuron', () => {
     expect(output).to.be.ok;
     expect(output.id).to.equal(`n_${layer.id}_4`);
   });
+  
+  it ('should get the neuron activated value', () => {
+    expect(n.A).to.be.equal(0);
+  });
+  
+  it ('should set the neuron activated value', () => {
+    n.A = 10;
+    expect(n.A).to.be.equal(10);
+  });
+  
+  it ('should get the neuron pre-activated value', () => {
+    expect(n.Z).to.be.equal(0);
+  });
+  
+  it ('should set the neuron pre-activated value', () => {
+    n.Z = 10;
+    expect(n.Z).to.be.equal(10);
+  });
+  
+  it ('should get the error inducted by the neuron', () => {
+    expect(n.delta).to.be.equal(undefined);
+  });
+  
+  it ('should set the error inducted by the neuron', () => {
+    n.delta = 10;
+    expect(n.delta).to.be.equal(10);
+  });
+  
+  it ('should add an input synapse to the neuron', () => {
+    let s = {
+      id: 1
+    };
+    
+    n.addInputSynapse(s);
+    expect(n.inputSynapses.length).to.be.equal(1);
+    expect(n.inputSynapses[0]).to.deep.equal(s);
+  });
+  
+  it ('should add an output synapse to the neuron', () => {
+    let s = {
+      id: 1
+    };
+    
+    n.addOutputSynapse(s);
+    expect(n.outputSynapses.length).to.be.equal(1);
+    expect(n.outputSynapses[0]).to.deep.equal(s);
+  });
+  
+  it ('should activate the neuron', () => {
+    let activationFunctions = {
+          activation: x => 2*x,
+          activationPrime: x => 2
+        },
+        s = {
+          id: 1,
+          weight: 0.5,
+          neurons: {
+            input: {
+              A: 1
+            }
+          }
+        };
+    
+    n.activate(activationFunctions);
+    expect(n.Z).to.be.equal(0);
+    expect(n.A).to.be.equal(0);
+    
+    n.addInputSynapse(s);
+    
+    n.activate(activationFunctions);
+    expect(n.Z).to.be.equal(0.5);
+    expect(n.A).to.be.equal(1);
+  });
+  
+  it ('should compute the neuron error', () => {
+    let cost = {
+          delta: (x, y) => (x + y)
+        },
+        s = {
+          id: 1,
+          weight: 0.5,
+          neurons: {
+            output: {
+              delta: 0.5
+            }
+          }
+        };
+        
+    n.computeError(cost);
+    expect(n.delta).to.be.equal(0);
+    
+    n.addOutputSynapse(s);
+    n.computeError(cost);
+    expect(n.delta).to.be.equal(0.25);
+  });
+  
+  it ('should back-propagate the neuron error to compute synapses gradients further', () => {
+    let s = {
+          id: 1,
+          weight: 0.5,
+          gradient: 0,
+          neurons: {
+            output: {
+              delta: 0.5
+            }
+          }
+        };
+        
+    n.A = 1;
+    n.addOutputSynapse(s);
+    n.backPropagate();
+    
+    expect(s.gradient).to.be.equal(0.5);
+  });
+  
 });
